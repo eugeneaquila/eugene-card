42b84ba41a30412ade4a983b1f5e74b07852c926
ainokori <eugene.aquila06@gmail.com>
2026-07-27 18:53:18 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.eugenecard.app',
   appName: 'Eugene Card Marketplace',
-  webDir: '\x16'
+  webDir: 'eugene-card-nine.vercel.app'
 };
 
 export default config;
