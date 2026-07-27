45de6e2422f29e740ec18d8f2cd2b220833c3a5e
ainokori <eugene.aquila06@gmail.com>
2026-07-27 17:00:10 +0800

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eugenecard.app',
+  appName: 'Eugene Card Marketplace',
+  webDir: '\x16'
+};
+
+export default config;
