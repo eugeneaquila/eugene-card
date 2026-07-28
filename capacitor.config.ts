6767fd98a6e6279cea7a06dd4633bae2d3ae2c95
ainokori <eugene.aquila06@gmail.com>
2026-07-28 11:04:50 +0800

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.eugenecard.app',
-  appName: 'Eugene Card Marketplace',
-  webDir: 'eugene-card-nine.vercel.app'
-};
-
-export default config;
